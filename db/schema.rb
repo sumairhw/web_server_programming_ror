49e22d6a79c08b85f3f4858de0eed9eef17a94b6
Sumair ul Hasan <sumair@epochclan.com>
2023-09-03 13:57:15 +0500

    Add ratings (model, controller, views)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_02_185631) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_03_084325) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_02_185631) do
   create_table "breweries", force: :cascade do |t|
     t.string "name"
     t.integer "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer "score"
+    t.integer "beer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
