14870b9d24f2ce01512f2c73f5dc301831947e56
Sumair ul Hasan <sumair@epochclan.com>
2023-09-15 23:15:53 +0500

    Add upto w2_ex13

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,4 +1,6 @@
 class RatingsController < ApplicationController
+  before_action :set_rating, only: %i[destroy]
+
   def index
     @ratings = Rating.all
   end
@@ -12,10 +14,19 @@ class RatingsController < ApplicationController
     @rating = Rating.create(rating_params)
   end
 
+  def destroy
+    @rating.delete
+    redirect_to ratings_path
+  end
+
   private
 
   def rating_params
     params.require(:rating).permit(:score, :beer_id)
     redirect_to ratings_path
+  end
+
+  def set_rating
+    @rating = Rating.find(params[:id])
   end
 end
