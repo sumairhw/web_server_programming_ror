a39d4de8508eb9f13d42829693abe6e20ba1f06b
Sumair ul Hasan <sumair@epochclan.com>
2023-09-24 15:44:10 +0500

    Add many to many rel bw beers & users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   validates :username, uniqueness: true,
                        length: { minimum: 3, maximum: 30 }
   has_many :ratings
+  has_many :beers, through: :ratings
 end
