fce5addc01b08cc79a12bc79d8e31d27539acc04
Sumair ul Hasan <sumair@epochclan.com>
2023-09-24 15:24:05 +0500

    w3e8: Add year validation

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -3,9 +3,18 @@ class Brewery < ApplicationRecord
 
   validates :name, presence: true
   validates :year, numericality: { only_integer: true,
-                                   greater_than_or_equal_to: 1040,
-                                   less_than_or_equal_to: 2022 }
+                                   greater_than_or_equal_to: 1040 }
+
+  validate :year_cannot_be_greater_than_current
 
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
+
+  private
+
+  def year_cannot_be_greater_than_current
+    return unless year.present? && year > Date.today.year
+
+    errors.add(:year, "cannot be greater than current year #{Date.today.year}")
+  end
 end
--- app/models/concerns/rating_average.rb
+++ app/models/concerns/rating_average.rb
@@ -2,6 +2,8 @@ module RatingAverage
   extend ActiveSupport::Concern
 
   def average_rating
+    return 0 if ratings.count == 0
+
     (ratings.sum(:score) / ratings.count).to_f
   end
 end
